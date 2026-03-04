051a57d5dda0852d6997b480a6c26e9bf5d2711c
tomaqed19991 <tomaqed1999@gmail.com>
2026-03-04 21:02:59 +0700

    Add custom label for actions

--- force-app/main/default/lwc/fec_RecallAction/fec_RecallAction.js
+++ force-app/main/default/lwc/fec_RecallAction/fec_RecallAction.js
@@ -1,10 +1,17 @@
 import { LightningElement, api } from 'lwc';
 import run from "@salesforce/apex/FEC_CaseBusinessService.run";
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-
+import FEC_Toast_Save_Success_Title from "@salesforce/label/c.FEC_Toast_Save_Success_Title";
+import FEC_Toast_Recall_Success from "@salesforce/label/c.FEC_Toast_Recall_Success";
+import FEC_Toast_Error from "@salesforce/label/c.FEC_Toast_Error";
 const RECALL = 'Recall';
 
 export default class Fec_RecallAction extends LightningElement {
+    label = {
+        FEC_Toast_Save_Success_Title,
+        FEC_Toast_Recall_Success,
+        FEC_Toast_Error
+    }
     @api recordId;
     @api invoke() {
         let params = {
@@ -13,10 +20,10 @@ export default class Fec_RecallAction extends LightningElement {
         
         run({ method: RECALL, params: params})
             .then(() => {
-                this.showToast('Success', 'Thực hiện Recall thành công.', 'success');
+                this.showToast(this.label.FEC_Toast_Save_Success_Title, this.label.FEC_Toast_Recall_Success, 'success');
             })
             .catch(error => {
-                this.showToast('Error', error.body.message, 'error');
+                this.showToast(this.label.FEC_Toast_Error, error.body.message, 'error');
             });
     }
 
--- force-app/main/default/lwc/fec_ReopenAction/fec_ReopenAction.js
+++ force-app/main/default/lwc/fec_ReopenAction/fec_ReopenAction.js
@@ -2,9 +2,17 @@ import { LightningElement, api } from 'lwc';
 import { updateRecord } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import run from "@salesforce/apex/FEC_CaseBusinessService.run";
+import FEC_Toast_Save_Success_Title from "@salesforce/label/c.FEC_Toast_Save_Success_Title";
+import FEC_Toast_Reopen_Success from "@salesforce/label/c.FEC_Toast_Reopen_Success";
+import FEC_Toast_Error from "@salesforce/label/c.FEC_Toast_Error";
 
 const REOPEN = 'Reopen';
 export default class Fec_ReopenAction extends LightningElement {
+    label = {
+        FEC_Toast_Save_Success_Title,
+        FEC_Toast_Reopen_Success,
+        FEC_Toast_Error
+    }
     @api recordId; 
     @api invoke() {
             let params = {
@@ -13,10 +21,10 @@ export default class Fec_ReopenAction extends LightningElement {
             
             run({ method: REOPEN, params })
                 .then(() => {
-                    this.showToast('Success', 'Thực hiện Reopen thành công.', 'success');
+                    this.showToast(this.label.FEC_Toast_Save_Success_Title, this.label.FEC_Toast_Reopen_Success, 'success');
                 })
                 .catch(error => {
-                    this.showToast('Error', error.body.message, 'error');
+                    this.showToast(this.label.FEC_Toast_Error, error.body.message, 'error');
                 });
         }
 
