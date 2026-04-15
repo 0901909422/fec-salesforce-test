0922a939ad6bcbd5ee10f36d0e7b8768a782013c
Linhdn16 <linhdn16@fpt.com>
2026-04-15 11:42:27 +0700

    Fix jira FECREDIT_CSM_2025_KH-942

--- force-app/main/default/lwc/fec_CaseBussiness/fec_CaseBussiness.js
+++ force-app/main/default/lwc/fec_CaseBussiness/fec_CaseBussiness.js
@@ -1674,6 +1674,13 @@ export default class Fec_CaseBussiness extends LightningElement {
       }
     }
 
+    const refundReqEl = this._getRefundRequestFormEl();
+    if (refundReqEl && typeof refundReqEl.validateRefund === "function") {
+      if (!refundReqEl.validateRefund()) {
+        isAllValid = false;
+      }
+    }
+
     // let accountContractField = this.template.querySelector(
     //   'lightning-input-field[data-field="' + FIELD_ACCOUNT_CONTRACT_NUMBER_PL + '"]',
     // );
@@ -1786,6 +1793,29 @@ export default class Fec_CaseBussiness extends LightningElement {
       return Promise.resolve();
     }
     return el.saveForSubmitIfApplicable();
+  }
+
+  _getRefundRequestFormEl() {
+    const wrap = this.template.querySelector(
+      '[data-fec-lwc="fec_RefundRequestForm"]',
+    );
+    const host = wrap && wrap.firstElementChild;
+    if (
+      host &&
+      (typeof host.validateRefund === "function" ||
+        typeof host.saveRefundDataIfVisible === "function")
+    ) {
+      return host;
+    }
+    return null;
+  }
+
+  _saveRefundRequestIfApplicable() {
+    const el = this._getRefundRequestFormEl();
+    if (!el || typeof el.saveRefundDataIfVisible !== "function") {
+      return Promise.resolve();
+    }
+    return el.saveRefundDataIfVisible();
   }
   /*Lấy element của form IPP Closure*/
   _getIppClosureFormEl() {
@@ -1962,6 +1992,7 @@ export default class Fec_CaseBussiness extends LightningElement {
       this._saveIPPClosureIfApplicable(),
       this._saveBeneficiaryIfApplicable(),
       this._saveCardClosureRefundForSubmitIfApplicable(),
+      this._saveRefundRequestIfApplicable(),
     ]);
     if (routeToEle) {
       let method = routeToEle.value;
