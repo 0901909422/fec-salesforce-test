85669b2a5078a24cc829122746d335ff306402e1
trunghieu98hnoi <hieutt25398@gmail.com>
2026-05-06 10:15:33 +0700

    fix comment

--- force-app/main/default/lwc/fec_AssignmentExecuteVisibility/fec_AssignmentExecuteVisibility.js
+++ force-app/main/default/lwc/fec_AssignmentExecuteVisibility/fec_AssignmentExecuteVisibility.js
@@ -1,26 +1,21 @@
 import { LightningElement, api, wire } from "lwc";
 import canExecute from "@salesforce/apex/FEC_AssignmentExecuteService.canExecuteAssignment";
-import { updateRecord } from "lightning/uiRecordApi";
+
 export default class Fec_AssignmentExecuteVisibility extends LightningElement {
-  @api recordId;
 
-  @wire(canExecute, { caseId: "$recordId" })
-  wiredResult({ data, error }) {
-    if (data !== undefined) {
-      this.updateField(data);
+    @api recordId;
+
+    canExecute;
+
+    @wire(canExecute, { caseId: "$recordId" })
+    wiredResult({ data, error }) {
+
+        if (data) {
+            this.canExecute = data.value;
+        }
+
+        if (error) {
+            console.error(error);
+        }
     }
-  }
-
-  async updateField(value) {
-    const fields = {
-      Id: this.recordId,
-      FEC_Can_Execute_Assignment__c: value,
-    };
-
-    try {
-      await updateRecord({ fields });
-    } catch (e) {
-      console.error(e);
-    }
-  }
 }
