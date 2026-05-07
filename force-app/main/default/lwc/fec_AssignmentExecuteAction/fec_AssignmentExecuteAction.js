f8852a878fc70cf11a412526c0d870b573460218
trunghieu98hnoi <hieutt25398@gmail.com>
2026-05-07 14:36:30 +0700

    fix execute assigment

--- force-app/main/default/lwc/fec_AssignmentExecuteAction/fec_AssignmentExecuteAction.js
+++ force-app/main/default/lwc/fec_AssignmentExecuteAction/fec_AssignmentExecuteAction.js
@@ -2,29 +2,42 @@ import { LightningElement, api, wire } from "lwc";
 import { publish, MessageContext } from "lightning/messageService";
 import { CloseActionScreenEvent } from "lightning/actions";
 
-import ASSIGNMENT_MODE
-from "@salesforce/messageChannel/FEC_Assignment_Mode__c";
+import ASSIGNMENT_MODE from "@salesforce/messageChannel/FEC_Assignment_Mode__c";
 
 export default class Fec_AssignmentExecuteAction extends LightningElement {
+  _recordId;
+  isPublished = false;
 
-    @api recordId;
+  @wire(MessageContext)
+  messageContext;
 
-    @wire(MessageContext)
-    messageContext;
+  @api
+  set recordId(value) {
+    this._recordId = value;
 
-    connectedCallback() {
-
-        publish(
-            this.messageContext,
-            ASSIGNMENT_MODE,
-            {
-                caseId: this.recordId,
-                isEditMode: true
-            }
-        );
-
-        this.dispatchEvent(
-            new CloseActionScreenEvent()
-        );
+    // publish only once when recordId is ready
+    if (value && !this.isPublished) {
+      this.handlePublishMessageChannel();
     }
+  }
+
+  get recordId() {
+    return this._recordId;
+  }
+
+  handlePublishMessageChannel() {
+    this.isPublished = true;
+
+    const payload = {
+      caseId: this.recordId,
+      isEditMode: true,
+    };
+
+    console.log("Publishing payload:", JSON.stringify(payload));
+
+    publish(this.messageContext, ASSIGNMENT_MODE, payload);
+
+    // close action after publish
+    this.dispatchEvent(new CloseActionScreenEvent());
+  }
 }
