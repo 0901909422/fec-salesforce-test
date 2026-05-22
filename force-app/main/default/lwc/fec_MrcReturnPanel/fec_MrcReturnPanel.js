fa78184cd34e2832353c535f72670e6aa896d83e
tungpv10 <pvmtunggg1998@gmail.com>
2026-05-22 15:02:54 +0700

    fix

--- force-app/main/default/lwc/fec_MrcReturnPanel/fec_MrcReturnPanel.js
+++ force-app/main/default/lwc/fec_MrcReturnPanel/fec_MrcReturnPanel.js
@@ -161,6 +161,10 @@ export default class Fec_MrcReturnPanel extends LightningElement {
     );
   }
 
+  @api getDeliveryForm() {
+    return this.template.querySelector("c-fec_-mrc-delivery-form");
+  }
+
   @api validateForSubmit() {
     if (
       (this.showDupBanner || this.showStandaloneDupBanner) &&
@@ -168,11 +172,27 @@ export default class Fec_MrcReturnPanel extends LightningElement {
     ) {
       return false;
     }
-    const delivery = this.template.querySelector("c-fec_-mrc-delivery-form");
-    if (delivery && typeof delivery.validateForComplete === "function") {
-      return delivery.validateForComplete();
+    const delivery = this.getDeliveryForm();
+    if (delivery && typeof delivery.validateForSubmit === "function") {
+      return delivery.validateForSubmit();
     }
     return true;
+  }
+
+  @api async saveToCase() {
+    const delivery = this.getDeliveryForm();
+    if (delivery && typeof delivery.saveToCase === "function") {
+      return delivery.saveToCase();
+    }
+    return { valid: true, messages: [] };
+  }
+
+  @api async saveDraftIfApplicable() {
+    const delivery = this.getDeliveryForm();
+    if (delivery && typeof delivery.saveDraftIfApplicable === "function") {
+      return delivery.saveDraftIfApplicable();
+    }
+    return { valid: true, messages: [] };
   }
 
   @api getHandlingOptionValue() {
