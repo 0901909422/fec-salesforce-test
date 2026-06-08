c6c7250064e09ebafb56eb132045288e3dd154fa
Phuong Nguyen The <phuongnt204@fpt.com>
2026-06-08 11:29:50 +0000

    Merge branch 'linhdev42_release-uat-3' into 'hotfix-uat'
    FECREDIT_CSM_2025_KH-1772, FECREDIT_CSM_2025_KH-1774, FECREDIT_CSM_2025_KH-1775, FECREDIT_CSM_2025_KH-1780, FECREDIT_CSM_2025_KH-1791
    
    See merge request fis-bank/bank_hcm/fecredit_csm_2025_th/fec_csm!3216

--- force-app/main/default/lwc/fec_customerAdditionalData/fec_customerAdditionalData.js
+++ force-app/main/default/lwc/fec_customerAdditionalData/fec_customerAdditionalData.js
@@ -1,5 +1,4 @@
-import { LightningElement, api, wire } from 'lwc';
-import { refreshApex } from '@salesforce/apex';
+import { LightningElement, api } from 'lwc';
 import getAdditionalDataRows from '@salesforce/apex/FEC_CustomerAdditionalDataController.getAdditionalDataRows';
 import FEC_ADDITIONAL_INFO_LABEL from '@salesforce/label/c.FEC_Additional_Info_Label';
 import { STR_EMPTY } from 'c/fec_CommonConst';
@@ -9,28 +8,37 @@ export default class FecCustomerAdditionalData extends LightningElement {
 
     cardTitle = FEC_ADDITIONAL_INFO_LABEL;
 
-    wiredResult;
     rows = [];
     error;
     loading = true;
 
-    @wire(getAdditionalDataRows, { caseId: '$recordId' })
-    wiredRows(value) {
-        this.wiredResult = value;
-        const { data, error } = value;
-        this.loading = false;
-        if (data) {
-            this.rows = data.map((r, idx) => ({
+    // 05/06/2026 18:00 linhdev - imperative call thay @wire: Apex không cacheable, luôn query mới khi load/refresh
+    connectedCallback() {
+        if (this.recordId) {
+            this.loadRows();
+        }
+    }
+
+    async loadRows() {
+        if (!this.recordId) {
+            this.rows = [];
+            this.loading = false;
+            return;
+        }
+        this.loading = true;
+        try {
+            const data = await getAdditionalDataRows({ caseId: this.recordId });
+            this.rows = (data || []).map((r, idx) => ({
                 ...r,
                 rowKey: (r.recordName || STR_EMPTY) + '-' + idx,
                 showFile: !!(r.sourceFileName && String(r.sourceFileName).trim())
             }));
             this.error = undefined;
-        } else if (error) {
+        } catch (error) {
             this.rows = [];
             this.error = error;
-        } else {
-            this.rows = [];
+        } finally {
+            this.loading = false;
         }
     }
 
@@ -55,10 +63,6 @@ export default class FecCustomerAdditionalData extends LightningElement {
 
     @api
     async refresh() {
-        if (this.wiredResult) {
-            this.loading = true;
-            await refreshApex(this.wiredResult);
-            this.loading = false;
-        }
+        await this.loadRows();
     }
 }
